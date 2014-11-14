233dcd826baab4da7fd9f9a1b0cf33032120176a
Anthony Romrell <anthony@artisandream.com>
2014-11-14 09:54:50 -0700

    Work on Random Firing.

---
+++ Assets/EnemyAmmoControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class EnemyAmmoControl : MonoBehaviour {
+
+	public EffectSettings myEffect;
+
+	public Action<EffectSettings> ResetAmmo;
+
+
+
+	void CallRest (EffectSettings _e)
+	{
+
+	}
+
+	void Awake () {
+		myEffect.ResetAmmo += CallRest;
+	}
+
+}
--- Assets/Realistic Effects Pack/Scripts/Share/EffectSettings.cs
+++ Assets/Realistic Effects Pack/Scripts/Share/EffectSettings.cs
@@ -7,6 +7,18 @@ public class EffectSettings : MonoBehaviour
 {
 
 	public Action<EffectSettings> ResetAmmo;
+	public GameObject[] targets;
+
+	public void SetTarget (EffectSettings _e) 
+	{
+
+	}
+
+	void Awake () 
+	{
+		ResetAmmo += SetTarget;
+	}
+
 
   public float ColliderRadius = 0.2f;
   public float EffectRadius = 0;
