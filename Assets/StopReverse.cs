034d6bbae8fd475d95a2131b639a419e407aa3df
Anthony Romrell <anthony@artisandream.com>
2015-02-04 14:58:42 -0700

    Added the ability to not reverse the character.

---
+++ Assets/StopReverse.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StopReverse : MonoBehaviour {
+
+	void OnTriggerExit () 
+	{
+		this.GetComponent<BoxCollider> ().isTrigger = false;
+		Debug.Log ("Exit");
+	}
+
+}
