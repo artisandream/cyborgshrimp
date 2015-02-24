2f35ff26c68673cac58e5796baece9b864d635bd
Anthony Romrell <anthony@artisandream.com>
2015-02-24 15:55:19 -0700

    Bug Fixes

---
+++ Assets/Scripts/GameControl/TurnOffBoxColliderAtEnd.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnOffBoxColliderAtEnd : MonoBehaviour {
+
+	void TurnOfComponents ()
+	{
+		this.GetComponent<BoxCollider>().enabled = false;
+	}
+
+	// Use this for initialization
+	void Start () {
+		EndGame.TurnOffGame += TurnOfComponents;
+	}
+	
+	void Update () {
+	
+	}
+}
