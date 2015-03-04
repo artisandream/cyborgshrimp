84603b7f6348b2c71bc7dc2b86794250fabde1a9
Anthony Romrell <anthony@artisandream.com>
2015-03-04 15:14:47 -0700

    Enemy Melee Kill works! Kills player.

---
+++ Assets/Scripts/AI/EnemyMelee.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMelee : MonoBehaviour {
+
+	public delegate void MeleeDel ();
+	public static MeleeDel MeleeEvent;
+
+	void OnMelee () {
+		if (MeleeEvent != null) {
+			MeleeEvent();
+		}
+	}
+}
---
+++ Assets/Scripts/AI/EnemyMeleeListener.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMeleeListener : MonoBehaviour {
+	
+	public ParticleSystem thisParticles;
+	public Vector3 randomPos;
+	public float randomDistance = 4;
+	public float heightOffset = 6;
+
+	IEnumerator EndParticalPlay ()
+	{
+		yield return new WaitForSeconds (0.01f);
+		thisParticles.Stop();
+	}
+
+	void AddDamage ()
+	{
+		randomPos.x = Random.Range (randomDistance * -1, randomDistance);
+		randomPos.y = Random.Range (0, randomDistance) + heightOffset;  
+		this.transform.localPosition = randomPos;
+		thisParticles.Play();
+		StartCoroutine (EndParticalPlay ());
+	}
+	
+	EnemyMeleeListener () {
+		EnemyMelee.MeleeEvent += AddDamage;
+	}
+}
